4f34d0d13089658e3ac7674be2853f6de5bcee42
not iron man <not.iron.yuriy@inbox.ru>
2023-06-01 14:59:34 +0300

    Create task_3.py

---
+++ 2. Class methods/task_3.py
@@ -0,0 +1,65 @@
+"""
+Имеется следующий класс для считывания информации из входного потока:
+
+import sys
+
+
+class StreamReader:
+    FIELDS = ('id', 'title', 'pages')
+
+    def readlines(self):
+        lst_in = list(map(str.strip, sys.stdin.readlines()))  # считывание списка строк из входного потока
+        sd = StreamData()
+        res = sd.create(self.FIELDS, lst_in)
+        return sd, res
+Которым, затем, можно воспользоваться следующим образом:
+
+sr = StreamReader()
+data, result = sr.readlines()
+Необходимо перед классом StreamReader объявить еще один класс StreamData с методом:
+
+def create(self, fields, lst_values): ...
+
+который бы на входе получал кортеж FIELDS из названий локальных атрибутов (передается в атрибут fields)
+и список строк lst_in (передается в атрибут lst_values) и формировал бы в объекте класса StreamData локальные свойства
+с именами полей из fields и соответствующими значениями из lst_values.
+
+Если создание локальных свойств проходит успешно, то метод create() возвращает True, иначе - False.
+Если число полей и число строк не совпадает, то метод create() возвращает False и локальные атрибуты создавать не нужно.
+
+P.S. В программе нужно дополнительно объявить только класс StreamData. Больше ничего делать не нужно.
+
+Пример входной информации (Sample Input):
+
+10
+Питон - основы мастерства
+512
+"""
+
+import sys
+
+
+class StreamData:
+    def create(self, fields: tuple[str], lst_values: list[int, str, int]) -> bool:
+        self.fields = fields
+        self.lst_values = lst_values
+
+        if len(fields) == len(lst_values):
+            self.__dict__ = dict(zip(self.fields, self.lst_values))
+            return True
+        else:
+            return False
+
+
+class StreamReader:
+    FIELDS = ('id', 'title', 'pages')
+
+    def readlines(self):
+        lst_in = list(map(str.strip, sys.stdin.readlines()))  # считывание списка строк из входного потока
+        sd = StreamData()
+        res = sd.create(self.FIELDS, lst_in)
+        return sd, res
+
+
+sr = StreamReader()
+data, result = sr.readlines()
