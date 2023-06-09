1392903719139ef1aeabd5abd16237d9f9d058c3
not iron man <not.iron.yuriy@inbox.ru>
2023-06-09 11:27:55 +0300

    Create task_3.py

---
+++ 2-2. Property/task_3.py
@@ -0,0 +1,61 @@
+"""
+Реализуйте класс Person, описывающий человека.
+При создании экземпляра класс должен принимать два аргумента в следующем порядке:
+
+name — имя человека
+surname — фамилия человека
+Экземпляр класса Person должен иметь два атрибута:
+
+name — имя человека
+surname — фамилия человека
+Класс Person должен иметь одно свойство:
+
+fullname — свойство, доступное для чтения и записи, возвращающее полное имя человека в виде строки: <имя> <фамилия>
+
+Примечание 1. При изменении имени и/или фамилии человека должно изменяться и его полное имя.
+Аналогично при изменении полного имени должны изменяться имя и фамилия.
+
+Примечание 2. Дополнительная проверка данных на корректность не требуется.
+Гарантируется, что реализованный класс используется только с корректными данными.
+"""
+
+
+class Person:
+    def __init__(self, name, surname):
+        self.name = name
+        self.surname = surname
+
+    def get_full(self):
+        return f'{self.name} {self.surname}'
+
+    def set_full(self, fullname):
+        self.name, self.surname = fullname.split()
+
+    fullname = property(get_full, set_full)
+
+
+if __name__ == '__main__':
+    person = Person('Меган', 'Фокс')
+    print(person.name)
+    print(person.surname)
+    print(person.fullname)
+
+    print('-' * 15)
+
+    person = Person('Меган', 'Фокс')
+    person.name = 'Стефани'
+    print(person.fullname)
+
+    print('-' * 15)
+
+    person = Person('Алан', 'Тьюринг')
+    person.surname = 'Вирт'
+    print(person.fullname)
+
+    print('-' * 15)
+
+    person = Person('Джон', 'Маккарти')
+
+    person.fullname = 'Алан Тьюринг'
+    print(person.name)
+    print(person.surname)
