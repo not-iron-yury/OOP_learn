5bf0d3722ef710cde7475d4aa99dd7aef4fce4e3
not iron man <not.iron.yuriy@inbox.ru>
2023-06-11 16:49:45 +0300

    Update task_6.py

--- 2-3. classmethod and staticmethod/task_6.py
+++ 2-3. classmethod and staticmethod/task_6.py
@@ -15,8 +15,43 @@ to_snake() — метод, который принимает в качестве
 
 to_upper_camel() — метод, который принимает в качестве аргумента строку в стиле Snake Case,
 записывает ее в стиле Upper Camel Case и возвращает полученный результат.
+
 """
+
+from re import fullmatch
+
 
 class CaseHelper:
     @staticmethod
     def is_snake(string: str) -> bool:
+        return bool(fullmatch('[a-z_]+', string))
+
+    @staticmethod
+    def is_upper_camel(string: str) -> bool:
+        return bool(fullmatch('([A-Z][a-z]+)+', string))
+
+    @staticmethod
+    def to_snake(string: str) -> str:
+        pass  # sub
+
+    @staticmethod
+    def to_upper_camel(string: str) -> str:
+        pass  # split
+
+
+if __name__ == '__main__':
+    print(CaseHelper.is_snake('beegeek'))
+    print(CaseHelper.is_snake('bee_geek'))
+    print(CaseHelper.is_snake('Beegeek'))
+    print(CaseHelper.is_snake('BeeGeek'))
+    print('-' * 15)
+    print(CaseHelper.is_upper_camel('beegeek'))
+    print(CaseHelper.is_upper_camel('bee_geek'))
+    print(CaseHelper.is_upper_camel('Beegeek'))
+    print(CaseHelper.is_upper_camel('BeeGeek'))
+    print('-' * 15)
+    print(CaseHelper.to_snake('Beegeek'))
+    print(CaseHelper.to_snake('BeeGeek'))
+    print('-' * 15)
+    print(CaseHelper.to_upper_camel('beegeek'))
+    print(CaseHelper.to_upper_camel('bee_geek'))
