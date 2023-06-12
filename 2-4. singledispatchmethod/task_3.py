595b12b522ccfcc84147f2ea1d7c329c235d789c
not iron man <not.iron.yuriy@inbox.ru>
2023-06-12 14:40:39 +0300

    Update task_3.py

--- 2-4. singledispatchmethod/task_3.py
+++ 2-4. singledispatchmethod/task_3.py
@@ -18,21 +18,54 @@ format() — метод, принимающий в качестве аргуме
 Примечание 4. Никаких ограничений касательно реализации класса Formatter нет, она может быть произвольной.
 """
 
+from functools import singledispatchmethod
 
 
+class Formatter:
 
+    @singledispatchmethod
+    @staticmethod
+    def format(val):
+        raise TypeError('Аргумент переданного типа не поддерживается')
 
+    @format.register(int)
+    @staticmethod
+    def _from_int(val):
+        print(f'Целое число: {val}')
 
+    @format.register(float)
+    @staticmethod
+    def _from_float(val):
+        print(f'Вещественное число: {val}')
+
+    @format.register(list)
+    @staticmethod
+    def _from_list(val):
+        values = ', '.join([f"\'{i}\'" if type(i) == str else str(i) for i in val])
+        print(f"Элементы списка: {values}")
+
+    @format.register(tuple)
+    @staticmethod
+    def _from_tuple(val):
+        values = ', '.join([f"\'{i}\'" if type(i) == str else str(i) for i in val])
+        print(f'Элементы кортежа: {values}')
+
+    @format.register(dict)
+    @staticmethod
+    def _from_dict(val):
+        print('Пары словаря: ' + ', '.join([str(i) for i in val.items()]))
 
 
 if __name__ == '__main__':
     Formatter.format(1337)  # Целое число: 1337
     Formatter.format(20.77)  # Вещественное число: 20.77
     Formatter.format([10, 20, 30, 40, 50])  # Элементы списка: 10, 20, 30, 40, 50
+    Formatter.format([10, 20, '30', '40', 50])  # Элементы списка: 10, 20, '30', '40', 50
     Formatter.format(([1, 3], [2, 4, 6]))  # Элементы кортежа: [1, 3], [2, 4, 6]
-    Formatter.format({'Cuphead': 1, 'Mugman': 3})  # Formatter.format({'Cuphead': 1, 'Mugman': 3})
-    Formatter.format({1: 'one', 2: 'two'})  # Formatter.format({1: 'one', 2: 'two'})
-    Formatter.format({1: True, 0: False})  # Formatter.format({1: True, 0: False})
+    Formatter.format(([1, '3'], [2, '4', 6]))  # Элементы кортежа: [1, 3], [2, 4, 6]
+    Formatter.format({'Cuphead': 1, 'Mugman': 3})  # Пары словаря: ('Cuphead', 1), ('Mugman', 3)
+    Formatter.format({1: 'one', 2: 'two'})  # Пары словаря: (1, 'one'), (2, 'two')
+    Formatter.format({1: True, 0: False})  # Пары словаря: (1, True), (0, False)
     try:
         Formatter.format('All them years, Dutch, for this snake?')
     except TypeError as e:
