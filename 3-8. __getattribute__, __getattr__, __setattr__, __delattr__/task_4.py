083ad43126b04a460c9b7bd1b89c9b87d6a39242
not iron man <not.iron.yuriy@inbox.ru>
2023-06-26 15:37:40 +0300

    Update task_4.py

--- 3-8. __getattribute__, __getattr__, __setattr__, __delattr__/task_4.py
+++ 3-8. __getattribute__, __getattr__, __setattr__, __delattr__/task_4.py
@@ -13,42 +13,24 @@
 Примечание 2. Никаких ограничений касательно реализации класса AttrsNumberObject нет, она может быть произвольной.
 """
 
-# Вариант 1
 class AttrsNumberObject:
     def __init__(self, **kwargs):
         self.__dict__.update(kwargs)
-        self.attrs_num = len(self.__dict__)
 
-    def __setattr__(self, key, value):
-        self.__dict__.update({key: value})
-        self.__dict__['attrs_num'] += 1
-
-    def __delattr__(self, attr):
-        del self.__dict__[attr]
-        self.__dict__['attrs_num'] -= 1
-
-
-# Вариант 2
-# class AttrsNumberObject:
-#     def __init__(self, **kwargs):
-#         self.__dict__.update(kwargs)
-#
-#     def __getattr__(self, name):
-#         if name == 'attrs_num':
-#             return len(self.__dict__) + 1
+    def __getattr__(self, name):
+        if name == 'attrs_num':
+            return len(self.__dict__) + 1
 
 
 if __name__ == '__main__':
     music_group = AttrsNumberObject(name='Alexandra Savior', genre='dream pop')
-
-    print(music_group.attrs_num)
+    print(music_group.attrs_num)    # 3
     del music_group.genre
-    print(music_group.attrs_num)
+    print(music_group.attrs_num)    # 2
 
     print('-' * 15)
 
     music_group = AttrsNumberObject(name='Woodkid', genre='pop')
-
-    print(music_group.attrs_num)
+    print(music_group.attrs_num)    # 3
     music_group.country = 'France'
-    print(music_group.attrs_num)
+    print(music_group.attrs_num)    # 4
